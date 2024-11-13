da87a13ce2a79ac7b7b5df3440a273387f5fdd0b
이경현 <lkh6035@naver.com>
2024-11-13 21:36:31 +0900

    [241113] add web to class 테스트 형식

---
+++ web/base.py
@@ -0,0 +1,21 @@
+import unittest
+from selenium import webdriver
+
+
+class Base(unittest.TestCase):
+    KBL_DOMAIN = "https://www.kbl.or.kr/"
+    IMPLICIT_WAIT_TIME = 3
+
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
+        options.add_argument("--start-maximized")
+
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.start_client()
+        self.driver.implicitly_wait(self.IMPLICIT_WAIT_TIME)
+        self.kbl_domain = self.KBL_DOMAIN
+
+    def tearDown(self):
+        pass
+        # self.driver.quit()
---
+++ web/login.py
@@ -0,0 +1,6 @@
+from base import Base
+
+
+# TODO: 로그인 테스트
+class Login(Base):
+    pass
