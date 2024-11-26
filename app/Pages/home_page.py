05b6a2f214a31d46e3b8f96ffc3f9e22addc280f
이경현 <lkh6035@naver.com>
2024-11-26 14:37:56 +0900

    [241126] add popup close 버튼, 팀 선택 버튼 추가(appium ui에서 xpath찾아서 진행

--- app/Pages/home_page.py
+++ app/Pages/home_page.py
@@ -5,7 +5,14 @@ from app.Pages.base_page import BasePage
 class HomePage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
-        self.popup_close_button_locator = (AppiumBy.XPATH, "//android.webkit.WebView")
+        self.popup_close_button_locator = (
+            AppiumBy.XPATH,
+            '//android.widget.Button[@text="Close"]',
+        )
+        self.team_select_button_locator = (
+            AppiumBy.XPATH,
+            '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View',
+        )
 
     def close_popup_if_present(self):
         try:
@@ -13,3 +20,7 @@ class HomePage(BasePage):
             popup_close_button.click()
         except:
             pass
+
+    def click_team_select_button(self):
+        team_select_button = self.get_element(self.team_select_button_locator)
+        team_select_button.click()
--- app/Tests/main_test.py
+++ app/Tests/main_test.py
@@ -7,3 +7,4 @@ class MainTest(BaseTest):
         home_page = HomePage(self.driver)
 
         home_page.close_popup_if_present()
+        home_page.click_team_select_button()
